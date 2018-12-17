c16e01e2b004b62291ddc86f353d126fdefb0960
Przemysław Jekiel <przemek.jekiel@gmail.com>
2018-12-17 20:14:50 +0100

    Test GUI, funkcja wczytujaca baze danych (xlsx)

---
+++ GUI.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt4 import QtGui , QtCore
+
+class Window(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle('Modele matematyczne procesow, podobienstwo i zmiana skali')
+        self.show()
+
+app = QtGui.QApplication(sys.argv)
+GUI = Window()
+sys.exit(app.exec_())
