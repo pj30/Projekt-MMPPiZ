e31592fa5506c9b3e53968bee48fa23bc1cc26e2
Przemysław Jekiel <przemek.jekiel@gmail.com>
2018-12-18 00:54:00 +0100

    MTP 2.0

--- MTP.py
+++ MTP.py
@@ -38,47 +38,48 @@ def load_data(file_name, sheet_name):
 
     return dict
 
-file_name = 'Database.xlsx'
+def solve():
+    file_name = 'Database.xlsx'
 
-# OGRANICZENIA
+    # OGRANICZENIA
 
-# Ilosc sztuk danego produktow K (wszystkich) w J magazynie
-#J,M = multidict({'Magazyn 1': 3000, 'Magazyn 2': 3000, 'Magazyn 3':3000})
-J,M = multidict(load_data(file_name, 'PojemnoscMagazynu'))
-# Jakie produkty sa dostepne w J magazynie
-produkt = load_data(file_name, 'DostepnoscProduktowWMagazynach')
+    # Ilosc sztuk danego produktow K (wszystkich) w J magazynie
+    #J,M = multidict({'Magazyn 1': 3000, 'Magazyn 2': 3000, 'Magazyn 3':3000})
+    J,M = multidict(load_data(file_name, 'PojemnoscMagazynu'))
+    # Jakie produkty sa dostepne w J magazynie
+    produkt = load_data(file_name, 'DostepnoscProduktowWMagazynach')
 
-# Zapotrzebowanie J klienta na K produktu
-d = load_data(file_name, 'Zapotrzebowanie')
+    # Zapotrzebowanie J klienta na K produktu
+    d = load_data(file_name, 'Zapotrzebowanie')
 
-# Lista sklepow
-I = set([i for (i,k) in d])
+    # Lista sklepow
+    I = set([i for (i,k) in d])
 
-# Lista produktow
-K = set([k for (i,k) in d])
-print(produkt)
-# Wagi produktow
-waga = load_data(file_name, 'WagaProduktu')
+    # Lista produktow
+    K = set([k for (i,k) in d])
+    print(produkt)
+    # Wagi produktow
+    waga = load_data(file_name, 'WagaProduktu')
 
-# Koszt transportu
-koszt = load_data(file_name, 'Koszt')
+    # Koszt transportu
+    koszt = load_data(file_name, 'Koszt')
 
-# Koszt dotarcia z J magazynu do M sklepu
-c = {}
-for i in I:
-    for j in J:
-        for k in produkt[j]:
-            c[i, j, k] = koszt[i,j] * waga[k] # Koszt*waga transportu z magazynu j do klienta i produktu k
+    # Koszt dotarcia z J magazynu do M sklepu
+    c = {}
+    for i in I:
+        for j in J:
+            for k in produkt[j]:
+                c[i, j, k] = koszt[i,j] * waga[k] # Koszt*waga transportu z magazynu j do klienta i produktu k
 
-# Model
-model = mctransp(I, J, K, c, d, M)
+    # Model
+    model = mctransp(I, J, K, c, d, M)
 
- # Optymalizacja
-model.optimize()
+    # Optymalizacja
+    model.optimize()
 
-EPS = 1.e-6
-x = model.data
-print("Ogolny koszt transportu wynosi:", model.getObjVal())
-for i,j,k in x:
-    if model.getVal(x[i,j,k]) > EPS:
-        print("Wysylka %10g sztuk %3s z magazynu %3s do klienta %3s" % (model.getVal(x[i,j,k]), k, j, i))
+    EPS = 1.e-6
+    x = model.data
+    print("Ogolny koszt transportu wynosi:", model.getObjVal())
+    for i,j,k in x:
+        if model.getVal(x[i,j,k]) > EPS:
+            print("Wysylka %10g sztuk %3s z magazynu %3s do klienta %3s" % (model.getVal(x[i,j,k]), k, j, i))
